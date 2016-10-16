cdd6f7675f48f7b5f50b127b86d6ad5955739819
eyupdalan <eyupdalan@gmail.com>
2016-10-16 21:56:13 +0300

    first commit

---
+++ src/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"encoding/json"
+)
+
+type Message struct {
+	Text string
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome %s", r.URL.Path[1:])
+}
+
+func about(w http.ResponseWriter, r *http.Request) {
+	m := Message{"Welcome to the my API"}
+	b, err := json.Marshal(m)
+
+	if (err != nil) {
+		panic(err)
+	}
+
+	w.Write(b)
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/about", handler)
+	http.ListenAndServe(":8080", nil)
+}
